754eb7ea8d82716275a501a2980e27a3d8cd8559
daivecns <daivecns@gmail.com>
2025-03-14 10:42:47 -0300

    Project creation

---
+++ SAE/SAE/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SAE
+//
+//  Created by Daive Simoes on 14/03/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ SAE/SAETests/SAETests.swift
@@ -0,0 +1,17 @@
+//
+//  SAETests.swift
+//  SAETests
+//
+//  Created by Daive Simoes on 14/03/25.
+//
+
+import Testing
+@testable import SAE
+
+struct SAETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
